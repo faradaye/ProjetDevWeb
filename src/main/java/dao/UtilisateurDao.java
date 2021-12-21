0cf3cfe022dfe75fa269a26c6cb562acec0968af
BIGEL Thomas <thomas.bigel4@etu.univ-lorraine.fr>
2021-12-21 21:35:00 +0100

    chiffrement mot de passe

--- src/main/java/dao/UtilisateurDao.java
+++ src/main/java/dao/UtilisateurDao.java
@@ -1,6 +1,7 @@
 package dao;
 
 import beans.Utilisateur;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -21,7 +22,7 @@ public class UtilisateurDao {
             connexion = daoFactory.getConnection();
             preparedStatement = connexion.prepareStatement("INSERT INTO Utilisateur(login, `password`, nom, prenom, date_naissance, administrateur) VALUES(?, ?, ?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS);
             preparedStatement.setString(1, utilisateur.getLogin());
-            preparedStatement.setString(2, utilisateur.getPassword()); //A hasher plus tard
+            preparedStatement.setString(2, BCrypt.hashpw(utilisateur.getPassword(), BCrypt.gensalt(12)));
             preparedStatement.setString(3, utilisateur.getNom());
             preparedStatement.setString(4, utilisateur.getPrenom());
             preparedStatement.setString(5, utilisateur.getDate_naissance().toString());
@@ -57,7 +58,7 @@ public class UtilisateurDao {
                     " SET login = ?, `password` = ?, nom = ?, prenom = ?, date_naissance = ?, administrateur = ? " +
                     " WHERE id = ?;", Statement.RETURN_GENERATED_KEYS);
             preparedStatement.setString(1, utilisateur.getLogin());
-            preparedStatement.setString(2, utilisateur.getPassword()); //A hasher plus tard
+            preparedStatement.setString(2, BCrypt.hashpw(utilisateur.getPassword(), BCrypt.gensalt(12)));
             preparedStatement.setString(3, utilisateur.getNom());
             preparedStatement.setString(4, utilisateur.getPrenom());
             preparedStatement.setString(5, utilisateur.getDate_naissance().toString());
@@ -104,7 +105,7 @@ public class UtilisateurDao {
             while (resultat.next()) {
                 int id = resultat.getInt("id");
                 String login = resultat.getString("login");
-                String password = resultat.getString("password");
+                String password = "";
                 String nom = resultat.getString("nom");
                 String prenom = resultat.getString("prenom");
                 Date date_naissance = resultat.getDate("date_naissance");
@@ -127,26 +128,27 @@ public class UtilisateurDao {
 
         try {
             connexion = daoFactory.getConnection();
-            preparedStatement = connexion.prepareStatement("SELECT * FROM Utilisateur WHERE login=? AND password=?;",
+            preparedStatement = connexion.prepareStatement("SELECT * FROM Utilisateur WHERE login=?;",
                     ResultSet.TYPE_SCROLL_SENSITIVE,
                     ResultSet.CONCUR_UPDATABLE);
             preparedStatement.setString(1, login);
-            preparedStatement.setString(2, password); //A hasher plus tard
 
             ResultSet resultat = preparedStatement.executeQuery();
             if (!resultat.isBeforeFirst()){
                 return  null;
             }
             else if (resultat.first()) {
-                int id = resultat.getInt("id");
-                String loginUtilisateur = resultat.getString("login");
-                String passwordUtilisateur = resultat.getString("password");
-                String nom = resultat.getString("nom");
-                String prenom = resultat.getString("prenom");
-                Date date_naissance = resultat.getDate("date_naissance");
-                Boolean administrateur = resultat.getBoolean("administrateur");
+                if(BCrypt.checkpw(password, resultat.getString("password"))){
+                    int id = resultat.getInt("id");
+                    String loginUtilisateur = resultat.getString("login");
+                    String passwordUtilisateur = resultat.getString("password");
+                    String nom = resultat.getString("nom");
+                    String prenom = resultat.getString("prenom");
+                    Date date_naissance = resultat.getDate("date_naissance");
+                    Boolean administrateur = resultat.getBoolean("administrateur");
 
-                utilisateur = new Utilisateur(id, loginUtilisateur, passwordUtilisateur, nom, prenom, date_naissance, administrateur);
+                    utilisateur = new Utilisateur(id, loginUtilisateur, passwordUtilisateur, nom, prenom, date_naissance, administrateur);
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -191,7 +193,7 @@ public class UtilisateurDao {
             if(resultat.next()){
                 int id = resultat.getInt("id");
                 String login = resultat.getString("login");
-                String password = resultat.getString("password");
+                String password = "";
                 String nom = resultat.getString("nom");
                 String prenom = resultat.getString("prenom");
                 Date date_naissance = resultat.getDate("date_naissance");
